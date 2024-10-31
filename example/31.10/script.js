6c098c694e554f44ed43decc1eaea96fd9a9c398
Nurila <nurilaakimhan11@gmail.com>
2024-10-31 12:26:46 +0500

    jumys

---
+++ example/31.10/script.js
@@ -0,0 +1,63 @@
+// console.log(result);
+
+
+
+//console.log(a-b); //10
+
+//var x = 45;
+//var y = 5;
+//var result = x * y; // 225
+
+//console.log(result);
+
+//var c = 20;
+//var d = 2;
+//console.log(20/2); //10
+
+//var a = 11; // tagaiyndau
+// ten ekenin tekseru
+///if (a % 2 = 0) { // = =
+// console.log(a, "sany jup san")}
+
+//var num = 100;
+//num ++; // increment
+//num--; // deccrement
+//console.log(num);
+
+//var a = 100;
+//a += 5; // qosyp tagaiyndau a = a + 5;
+//console.log(a);
+
+//var a = 100;
+//a -= 5; // a = a - 5;
+//a /= 5; // a = a / 5;
+//a *= 5; // a = a * 5;
+//a %= 3; // a = a % 3; a = 1;
+//console.log(a);
+
+//var a = 10;
+//var b = 20;
+//var c = 5;
+//var d = 5;
+//var e = 5;
+
+//let total = (a + b) * c - d / e;
+ //console.log(total);
+
+ //let a = 45;
+ //let b = 40;
+ //let c = 42;
+ //let z = 4;
+ //let x = 47;
+ //let a = 45;
+
+ //let total = ((a*b)/c)/(z-3);
+//console.log(total);
+
+//let a = 45;
+//let b = 40;
+//let c = 42;
+
+//let result = (((a/b)*45)/(c-b-a/10)+56);
+//console.log(result);
+
