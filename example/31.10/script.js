915003179d0ee3650cebf05b071d54d75d9da1f1
Nurila <nurilaakimhan11@gmail.com>
2024-10-31 12:50:05 +0500

    sabak

--- example/31.10/script.js
+++ example/31.10/script.js
@@ -44,14 +44,14 @@
 //let total = (a + b) * c - d / e;
  //console.log(total);
 
- let a = 45;
- let b = 40;
- let c = 42;
- let z = 4;
- let x = 47;
+ //let a = 45;
+//let b = 40;
+ //let c = 42;
+ //let z = 4;
+ //let x = 47;
 
- let total = ((a*b)/c)/(z-3);
-console.log(total % 2);
+ //let total = ((a*b)/c)/(z-3);
+//console.log(total % 2);
 
 //let a = 45;
 //let b = 40;
@@ -59,4 +59,26 @@ console.log(total % 2);
 
 //let result = (((a/b)*45)/(c-b-a/10)+56);
 //console.log(result);
+
+//var x = 105; //tagaiyndau
+//var y = 40;
+// salystyru ortalygy
+//if (x>y) {
+    //console.log(x, "ulkenirek")
+//}
+//else {
+    //console.log(y, "ulkenirek")
+//}
+
+var x = 100;
+// = = ten ekenin tekseru
+if (x === 100){
+    console.log("Keremet, jarasyn");
+} else if(x > 80){ //> = 
+    console.log("Jaman emes!");
+}else if(x < 50){ // < = 
+    console.log("Tagy oqu kerek");
+}else{
+    console.log("???");
+}
 
