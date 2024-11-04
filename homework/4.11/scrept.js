463fc12e69acdfb52aec99cd8d1fbbf2d08edbbc
Nurila <nurilaakimhan11@gmail.com>
2024-11-04 23:30:15 +0500

    ui jumys

---
+++ homework/4.11/scrept.js
@@ -0,0 +1,16 @@
+var age = 17;
+console.log(age >= 16);
+
+var num = 10;
+if (num % 2 == 0) {
+    console.log("Taq");
+}
+
+var temperature = 25;
+console.log(temperature >= 20 && temperature <= 30);
+
+var n = 5;
+console.log(n >= 0);
+
+var a = 15;
+console.log(a >= 5 && 10 <= a);
