5d04530bd81ad2ce895be4592c01d76995bde035
Nurila <nurilaakimhan11@gmail.com>
2024-11-06 00:52:02 +0500

    ui jum

---
+++ homework/5.11/script.js
@@ -0,0 +1,17 @@
+var birthYea = prompt("How old are you? ");
+
+if (birthYea >= 18){ 
+    alert("Sizdin jasynyz " + 18 + "den asqan");
+}
+if(birthYea <= 18){
+    alert("Sizdin jasynyz " + 18 + "ge tolmagan");
+}
+
+var temperature = prompt("ne kiemin?");
+
+if (temperature <= -10){
+    alert("shuba, gamazh, taqiya kigen durys");
+}
+if(temperature >= +20){
+    alert("shorty, fudbolka, kepka kigen durys");
+}
