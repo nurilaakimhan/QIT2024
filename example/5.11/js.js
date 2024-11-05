3572b4f639bf21682ae06a93a26d4174a63be5b7
Nurila <nurilaakimhan11@gmail.com>
2024-11-05 12:54:50 +0500

    kl

---
+++ example/5.11/js.js
@@ -0,0 +1,63 @@
+//alert("Hello world! " + "Hello" + "\n" + "2024");
+//prompt("How are you?");
+//let name = prompt("Enter your name");
+//let age = prompt("Enter your age:");
+//let email = prompt("Enter your email");
+
+//alert("Hello, " + name + "\n" + "You are" + "\n" + age + "\n" + "yaers old" + "\n" + "Yur" + email);
+
+//var num = prompt("Enter any namber: "); //san engizu
+
+//if (num % 2 ==0){ // jup ekenin tekseru
+//    alert("Number " + num + "is even");
+//}else{
+ //   alert("Number " + num + "is odd");
+//}
+
+//var birthYear = Number(prompt("Enter year of birth:"));
+//var age = "Your age " + (2024 - birthYear);
+
+//if (isNaN(birthYear)) {
+//    alert("Enter number");
+//}else{
+//    alert(age);
+//}
+
+//var password = prompt("Enter a password");
+//var correctPassword = ("nurila");
+
+//if(password == correctPassword){
+ //   alert("Registration is done");
+//}else{
+ //   alert("Password don't match");
+//}
+
+//var score = 0;
+//var userAns1 = prompt("IT agaidyn aty kim?" + "\n" + "A)" + "Temirgali" + "\n" + "B)" + "Temirlan");
+
+//if(userAns1 == "A"|| userAns1 == "B"){
+ //   score++;
+ //   alert("+ 1 upai");
+//}else{
+ //   alert("Qate");
+//}
+
+//var userAns2 = prompt("English apaidyn aty kim?");
+
+//if(userAns2 == "Dilnaz"|| userAns2 == "Dilnaz apai"){
+//    score++;
+//    alert("+ 1 upai");
+//}else{
+ //   alert("Qate!");
+//}
+
+//alert(score)
+
+
+
+var temperature = prompt (
+    "Enter temperature in Farenheit: "
+);
+var celsius = temperature/ 33.8;
+ 
+alert(Math.round(celsius));
